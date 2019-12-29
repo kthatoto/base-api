345a4cb8b7990d4e60551afbf926197b156eab2a
kthatoto <kthatoto@gmail.com>
2019-12-30 01:26:16 +0900

    Rename empty env file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,8 @@ class ApplicationController < ActionController::API
     def current_user
       return @current_user if @current_user
       return nil unless token = request.headers['Authorization']
+      client = Firebase::Auth::Client.new(ENV['FIREBASE_APIKEY'])
       token.gsub!('Bearer ', '')
-      pp token
+      pp client.get_account_info(token)
     end
 end
