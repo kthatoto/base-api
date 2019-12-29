fef74cc66b6f8b0feecdb88098d560e2d2c99635
kthatoto <kthatoto@gmail.com>
2019-12-30 04:20:58 +0900

    Add user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,9 @@ class ApplicationController < ActionController::API
       return nil unless token = request.headers['Authorization']
       client = Firebase::Auth::Client.new(ENV['FIREBASE_APIKEY'])
       token.gsub!('Bearer ', '')
-      pp client.get_account_info(token)
+      res = client.get_account_info(token)
+      return nil unless res.success?
+      email = res.body['users'].first['email']
+      @current_user = User.find_or_create_by(email: email)
     end
 end
